4c8051faa115e23410454493505727db87811efe
Сергей Разуваев <razuvaev.sd@ya.ru>
2022-03-31 03:56:54 +1100

    action 'me' added

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 from users.models import User
 
 from .serializers import UserSerializer
@@ -8,3 +11,16 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'username'
+
+    @action(methods=['get', 'patch'], detail=False)
+    def me(self, request):
+        instance = get_object_or_404(User, pk=request.user)
+
+        if request.method == 'GET':
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        serializer = UserSerializer(instance=instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
