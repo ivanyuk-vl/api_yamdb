0305b1f81669e1327ab8240789089caf09ce11f1
Влад Иванюк <ivanyuk.vl@yandex.ru>
2022-03-30 21:13:40 +1000

    urls updated

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,16 +4,16 @@ from rest_framework import routers
 from .views import CommentViewSet, ReviewViewSet, TitleViewSet
 
 
-router = routers.DefaultRouter()
-router.register('titles', TitleViewSet, basename='title')
-router.register(
+router_v1 = routers.DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='title')
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comment',
 )
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
