93732237c9075b6d819bbe5e88e6b133159a47d5
Сергей Разуваев <razuvaev.sd@ya.ru>
2022-04-06 23:22:37 +1100

    test

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,17 +2,10 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser, AnonymousUser
 from django.db import models
 
-
-class UserRole:
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-
-
 ROLES = (
-    (UserRole.USER, 'Аутентифицированный пользователь'),
-    (UserRole.MODERATOR, 'Модератор'),
-    (UserRole.ADMIN, 'Администратор'),
+    ('user', 'Аутентифицированный пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
 )
 
 
