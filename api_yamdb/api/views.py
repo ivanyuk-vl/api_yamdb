2009f4a209ff2403a921686892c64501299aea40
Влад Иванюк <ivanyuk.vl@yandex.ru>
2022-04-06 23:38:18 +1000

    fixed #19

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -121,7 +121,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAdminOrIsModeratorOrIsAuthorOrReadOnly,)
 
     def get_review(self):
-        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'),
+            title__id=self.kwargs.get('title_id')
+        )
 
     def get_queryset(self):
         return self.get_review().comments.all()
