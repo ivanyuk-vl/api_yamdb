e04ffb05247338097f1b07ba83345657ac995c97
Влад Иванюк <ivanyuk.vl@yandex.ru>
2022-04-07 23:48:14 +1000

    fixed #30, fixed #25

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Category, Comment, Genre, Review, GenreTitle, Title
+from reviews.models import Category, Comment, Genre, Review, Title
 from api_yamdb.settings import MAX_SCORE, MIN_SCORE
 from users.models import ROLES, User
 
@@ -94,13 +94,6 @@ class TitleSerializer(TitleGetSerializer):
     category = SlugRelatedField(
         queryset=Category.objects.all(), slug_field='slug'
     )
-
-    def create(self, validated_data):
-        genres = validated_data.pop('genre')
-        title = Title.objects.create(**validated_data)
-        for genre in genres:
-            GenreTitle.objects.create(title=title, genre=genre)
-        return title
 
 
 class ReviewSerializer(serializers.ModelSerializer):
