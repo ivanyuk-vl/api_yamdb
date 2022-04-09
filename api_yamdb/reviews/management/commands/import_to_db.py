2adaf683f6e0e03c0d522a91ca0a5f42166664a0
Сергей Разуваев <razuvaev.sd@ya.ru>
2022-04-09 14:34:06 +1100

    Change manage-command fixes #32

--- api_yamdb/reviews/management/commands/import_to_db.py
+++ api_yamdb/reviews/management/commands/import_to_db.py
@@ -2,7 +2,6 @@ import csv
 
 from django.apps import apps
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 
 
@@ -32,29 +31,47 @@ def clear_model(model_class):
                         f'возникла ошибка: {ex}')
 
 
+def replace_relation_fields_with_instance(record, model_class):
+    """замена в словаре идентификатора реляционного объекта его экземпляром"""
+    for field in model_class._meta.fields:
+        # Если в файле присутствует реляционное поле
+        if field.is_relation and field.name in record:
+            # Проверить наличие объекта в модели
+            # по передаваемому значению из файла
+            if not field.related_model.objects.filter(
+                    pk=record[field.name]).exists():
+                raise ValueError(
+                    'Объект c идентификатором '
+                    f'{field.name}={record[field.name]}, '
+                    f'принадлежащий модели {field.related_model}'
+                    ' не найден.')
+            # при наличии объекта получить его инстанс
+            related_object = field.related_model.objects.get(
+                pk=record[field.name])
+            # замещаем в данных из идентификатор объекта
+            # полученным инстансом
+            record[field.name] = related_object
+    return record
+
+
 def create_objects(model_class, file_path):
     """Запись данных из файла"""
     with open(file_path, encoding='UTF-8') as file:
         csv_reader = csv.DictReader(file)
-        model_fields = [field.name
-                        for field in model_class._meta.fields]
-
-        if not set(csv_reader.fieldnames) <= set(model_fields):
-            raise ValidationError('Набор полей в файле превышает'
-                                  'набор полей модели')
-
         objs = []
         for value in csv_reader:
+            record = replace_relation_fields_with_instance(
+                dict(value), model_class)
             try:
                 # создать экземпляр объекта из строки файла
-                objs.append(model_class(**dict(value)))
+                objs.append(model_class(**record))
             except Exception as ex:
                 raise Exception('Ошибка создания'
                                 f'экземпляра объекта: {ex}')
         try:
             # записать объекты в базу
-            obj = model_class.objects.bulk_create(objs, len(objs))
-            return obj
+            return model_class.objects.bulk_create(objs, len(objs))
+
         except Exception as ex:
             raise Exception(f'Ошибка звписи объектов в базу: {ex}')
 
@@ -87,6 +104,5 @@ class Command(BaseCommand):
                 + (options['filename'] or (options['model'] + '.csv'))
             )
             # записать данные в модель из файла
-            objs = create_objects(model_class, file_path)
-            if objs:
+            if create_objects(model_class, file_path):
                 self.stdout.write('Запись в модель данных успешно выполнена')
