4322edb83850b4be2b80bf2d3be5a47f1c9ba381
Сергей Разуваев <razuvaev.sd@ya.ru>
2022-04-10 14:22:35 +1100

    fixes #43

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -44,7 +44,7 @@ class TokenSerializer(serializers.Serializer):
 
 
 class SignUpSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.EmailField(max_length=254)
     username = serializers.CharField(max_length=150)
     confirmation_code = serializers.CharField(required=False, write_only=True)
 
